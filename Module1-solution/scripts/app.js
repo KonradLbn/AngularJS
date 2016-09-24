e159f190514d6e63c52d9a603b887a5f60f674c0
Konrad Niderla <konrad.niderla@gmail.com>
2016-09-24 22:57:26 +0200

    Module1 finishes

--- Module1-solution/scripts/app.js
+++ Module1-solution/scripts/app.js
@@ -2,50 +2,47 @@
     'use strict';
 
     angular.module('LunchCheck', [])
+        .controller('LunchCheckController', LaunchCheckController)
 
-        .controller('LunchCheck', function ($scope) {
-            $scope.dishesList = "";
-            $scope.returnMessage = "";
+    LaunchCheckController.$inject = ['$scope'];
+    function LaunchCheckController($scope) {
+        $scope.dishesList = "";
+        $scope.returnMessage = "";
 
-            /*  
-              $scope.displayNumeric = function () {
-                  var totalNameValue = calculatNumericForString($scope.name);
-                  $scope.totalValue = totalNameValue;
-              };
-            */
+        $scope.CheckIfTooMuch = function () {
 
-            $scope.CheckIfTooMuch = function () {
+            $scope.returnMessage = CreateMessage(CountDishes($scope.dishesList));
+        }
 
-                $scope.returnMessage = CreateMessage(CountDishes($scope.dishesList));
-            }
+        function CountDishes(msg) {
+            var re = /\s*,\s*/;
+            var lst = msg.split(re);
+            lst = CleanArray(lst);
+            return lst.length
+        }
 
-            function CountDishes(msg) {
-                var re = /\s*,\s*/;
-                var lst = msg.split(re);
-                lst = CleanArray(lst);
-                return lst.length
-            }
-
-            function CleanArray(actual) {
-                var newArray = new Array();
-                for (var i = 0; i < actual.length; i++) {
-                    if (actual[i]) {
-                        newArray.push(actual[i]);
-                    }
-                }
-                return newArray;
-            }
-
-            function CreateMessage(dishesCount) {
-                if (dishesCount <= 3) {
-                    return "Enjoy!";
-                }
-                else {
-                    return "Too much!";
+        function CleanArray(actual) {
+            var newArray = new Array();
+            for (var i = 0; i < actual.length; i++) {
+                if (actual[i]) {
+                    newArray.push(actual[i]);
                 }
             }
+            return newArray;
+        }
 
-        });
+        function CreateMessage(dishesCount) {
+            if (dishesCount == 0) {
+                return "Please enter data first";
+            }
+            else if (dishesCount <= 3) {
+                return "Enjoy!";
+            }
+            else {
+                return "Too much!";
+            }
+        }
+    };
 
 
 })();
